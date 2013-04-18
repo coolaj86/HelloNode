308658d8c04b67892f02885b464809fdda2edab2
AJ ONeal <coolaj86@gmail.com>
2013-04-18 15:57:49 -0600

    updated grunt stuff and added public/

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,26 +2,14 @@ module.exports = function (grunt) {
   "use strict";
 
   grunt.initConfig({
-      "watch": {
-          "all": {
-              "files": [ "./browser/**.less", "./browser/**.jade", "./browser/**.js" ]
-            , "tasks": [ "jade:dev", "less:dev", "pakmanager:browser" ]
-          }
-      }
-    , "develop": {
-          "server": {
-              "file": "bin/server.js"
-          }
-      }
-    , "jade": {
+      jade: {
           "dev": {
               "files": {
                   "public/index.html": "./browser/index.jade"
-                , "public/gcf.html": "./browser/gcf.jade"
               }
           }
       }
-    , "less": {
+    , less: {
           "dev": {
               "files": {
                   "public/style.css": "./browser/style.less"
@@ -31,16 +19,16 @@ module.exports = function (grunt) {
     , copy: {
           main: {
               files: [
-                  { src: 'browser/app.js', dest: 'public/'}
-                , { src: 'browser/jquery.js', dest: 'public/'}
+                  { expand:true, cwd: 'browser', src: ['app.js'], dest: 'public/'}
+                , { expand:true, cwd: 'browser', src: ['jquery.js'], dest: 'public/'}
               ]
           }
       }
   });
-  // uglify
+
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-jade');
-  grunt.loadNpmTasks('grunt-copy');
-  grunt.registerTask('build', ["jade:dev", "less:dev", "pakmanager:browser", "copy:main"]);
-  grunt.registerTask('default', ['build', 'watch']);
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.registerTask('build', ["jade:dev", "less:dev", "copy:main"]);
+  grunt.registerTask('default', ['build']);
 };
