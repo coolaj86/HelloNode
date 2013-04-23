87e53154db305d682d53131dc1a91869aadec17a
AJ ONeal <coolaj86@gmail.com>
2013-04-22 22:35:24 -0600

    commented out stuff about firefox

--- winstaller/winstart.bat
+++ winstaller/winstart.bat
@@ -1,19 +1,21 @@
 :: Run a local node instance if it exists, otherwise a global node instance
 :: %~dp0 is the directory where this file is located, similar to node's __dirname
 :: Explanation of %~dp0 http://stackoverflow.com/a/5034119/151312
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe" "%~dp0\.\bin\server.js" %*
-) ELSE (
-  node "%~dp0\.\bin\server.js" %*
-)
+::@IF EXIST "%~dp0\node.exe" (
+::  "%~dp0\node.exe" "%~dp0\.\bin\server.js" %*
+::) ELSE (
+::  node "%~dp0\.\bin\server.js" %*
+::)
+
 :: Open App in Chrome or another application
 @IF EXIST "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" (
   "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --app=http://localhost:5566 --use-data-dir=%APPDATA%\HelloNode\
 ) ELSE (
-  IF EXIST "C:\Program Files (x86)\Mozilla Firefox\firefox.exe" (
-  :: Thanks to http://support.mozilla.org/en-US/questions/955250
-  "C:\Program Files (x86)\Mozilla Firefox\firefox.exe" -app %~dp0\firefox-app.ini
-  ) ELSE (
+::  IF EXIST "C:\Program Files (x86)\Mozilla Firefox\firefox.exe" (
+  :: Need to read more http://support.mozilla.org/en-US/questions/955250
+  :: https://developer.mozilla.org/en-US/docs/Getting_started_with_XULRunner
+::  "C:\Program Files (x86)\Mozilla Firefox\firefox.exe" -app %~dp0\firefox-app.ini
+::  ) ELSE (
     start %~dp0\msie-app.hta
-  )
+::  )
 )
