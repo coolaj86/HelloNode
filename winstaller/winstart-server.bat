3c6a0c868e3d7a574e7bdd7cce444bba6b8676f5
AJ ONeal <coolaj86@gmail.com>
2013-04-23 02:12:43 -0600

    fixed mismatched names of winstart-*.bat

--- winstaller/winstart-server.bat
+++ winstaller/winstart-server.bat
@@ -1,15 +1,8 @@
 :: Run a local node instance if it exists, otherwise a global node instance
 :: %~dp0 is the directory where this file is located, similar to node's __dirname
-
-:: Open App in Chrome or another application
-@IF EXIST "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" (
-  "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --app=http://localhost:5566 --use-data-dir=%APPDATA%\HelloNode\
+:: Explanation of %~dp0 http://stackoverflow.com/a/5034119/151312
+@IF EXIST "%~dp0\node.exe" (
+  "%~dp0\node.exe" "%~dp0\.\bin\server.js" %*
 ) ELSE (
-::  IF EXIST "C:\Program Files (x86)\Mozilla Firefox\firefox.exe" (
-  :: Need to read more http://support.mozilla.org/en-US/questions/955250
-  :: https://developer.mozilla.org/en-US/docs/Getting_started_with_XULRunner
-::  "C:\Program Files (x86)\Mozilla Firefox\firefox.exe" -app %~dp0\firefox-app.ini
-::  ) ELSE (
-    start %~dp0\msie-app.hta
-::  )
+  node "%~dp0\.\bin\server.js" %*
 )
