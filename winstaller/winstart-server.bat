082247833617cd9fbd100035fc812a0dfc520c28
AJ ONeal <coolaj86@gmail.com>
2013-04-23 02:31:34 -0600

    fixed CRLF

--- winstaller/winstart-server.bat
+++ winstaller/winstart-server.bat
@@ -1,6 +1,7 @@
-:: Run a local node instance if it exists, otherwise a global node instance
 :: %~dp0 is the directory where this file is located, similar to node's __dirname
 :: Explanation of %~dp0 http://stackoverflow.com/a/5034119/151312
+
+:: Run a local node instance if it exists, otherwise a global node instance
 @IF EXIST "%~dp0\node.exe" (
   "%~dp0\node.exe" "%~dp0\.\bin\server.js" %*
 ) ELSE (
