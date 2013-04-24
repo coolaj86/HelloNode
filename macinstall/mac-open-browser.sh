0872c1d65cecb1ce31b0b40567b8f2947b5585e0
AJ ONeal <coolaj86@gmail.com>
2013-04-23 20:33:11 -0600

    don't use tilde for home

--- macinstall/mac-open-browser.sh
+++ macinstall/mac-open-browser.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome \
   --app=http://localhost:5566 \
-  --user-data-dir=~/.nodejsapps/chromeprofile &
+  --user-data-dir=${HOME}/.nodejsapps/chromeprofile &
